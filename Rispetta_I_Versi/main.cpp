25807d62f420b6e1d25cdf40bac785ec8b6986a3
luigiariano <85135517+luigiariano@users.noreply.github.com>
2023-10-08 03:13:32 +0200

    Add files via upload

---
+++ Rispetta_I_Versi/main.cpp
@@ -0,0 +1,22 @@
+#include "metodi.h"
+
+int main() {
+/*
+string p1="problema.txt";
+string s1="soluzione.txt";
+string e1="esito.txt";
+//andiamo ad aprire i nostri 3 file e nel file problema.txt scriviamo dei problemi
+    fstream p2,s2,e2; //creo i due oggetti che mi serviranno per creare il file del problema e della soluzione
+    p2.open(p1.c_str(),ios::in | ios::out | ios::trunc);
+    s2.open(s1.c_str(),ios::in | ios::out | ios::trunc);
+    e2.open(e1.c_str(),ios::in | ios::out);
+    //vado a scrivere nel file del problema i problemi
+    p2<<"<<"<<endl<<"<<<"<<endl<<"<<<<<<"<<endl; int n=3;
+    //Ora diamo la possibilità all'utente di risolvere i problemi
+string esercizio="<<<";
+ */
+  rispetta_i_versi ciao;
+  ciao.crea_file(ciao);
+
+    return 0;
+}
